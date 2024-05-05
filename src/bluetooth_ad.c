757ac5b1c65d7b2498081e9986912f107185eb31
Lee Junhee <junhee0110@gmail.com>
2024-05-05 17:33:52 +0900

    [EDIT] bluetooth_ad
    로깅 추가

--- src/bluetooth_ad.c
+++ src/bluetooth_ad.c
@@ -1,10 +1,16 @@
 #include "bluetooth_ad.h"
 #include "zephyr/bluetooth/bluetooth.h"
 #include "zephyr/bluetooth/gap.h"
-#ifdef CONFIG_LOG
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(bluetooth_ad, LOG_LEVEL_INF);
-#endif
+
+static struct bt_le_adv_param *adv_param = BT_LE_ADV_PARAM(
+	(//BT_LE_ADV_OPT_CONNECTABLE |
+	 BT_LE_ADV_OPT_USE_IDENTITY), /* Connectable advertising and use identity address */
+	BT_GAP_ADV_FAST_INT_MIN_1, /* 0x30 units, 48 units, 30ms */
+	BT_GAP_ADV_FAST_INT_MAX_1, /* 0x60 units, 96 units, 60ms */
+	NULL); /* Set to NULL for undirected advertising */
+
 
 static struct bt_data ad[] = {
     BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR | BT_LE_AD_GENERAL),
@@ -15,8 +21,6 @@ static struct bt_data sd[] = {
     BT_DATA(BT_DATA_NAME_SHORTENED, "Roll4", 5)
 };
 
-static struct bt_le_adv_param *adv_param;
-
 int start_bt_advertise() {
     if(!bt_is_ready()) {
         LOG_INF("Bluetooth is not ready. Enable Bluetooth");
@@ -24,7 +28,8 @@ int start_bt_advertise() {
         if (err < 0) {
             return err;
         }
+        LOG_INF("Bluetooth Enabled");
     }
 
-    return bt_le_adv_start(BT_LE_ADV_NCONN, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));   
+    return bt_le_adv_start(adv_param, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));   
 }
