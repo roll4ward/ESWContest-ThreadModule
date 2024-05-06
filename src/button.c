74f0678f47abd69247c26e45f9afc4a5650b5945
Lee Junhee <junhee0110@gmail.com>
2024-05-06 23:09:11 +0900

    [EDIT] button - 블루투스 버튼 상태 이벤트
    - 버튼 누른 시간이 1초를 넘으면 길게 누른것으로 판단
      - LONG_PRESS 이벤트 설정
    - 1초가 되기 전에 버튼을 놓으면 짧게 누른 것으로 판단
      - SHORT_PRESS 이벤트 설정
    - 버튼을 놓으면 RELEASE 이벤트 설정

--- src/button.c
+++ src/button.c
@@ -5,18 +5,25 @@
 #define     BTN_WORKQUEUE_PRIORITY                   5
 #define     BTN_QUEUE_STACK_SIZE                     512
 #define     BT_BUTTON_NODE                           DT_ALIAS(bluetooth_button)
+#define     LONG_PRESS_BOUNDARY_MS                   1000LL
+
+#define     BT_BUTTON_RELEASE                        BIT(0)
+#define     BT_BUTTON_SHORT_PRESS                    BIT(1)
+#define     BT_BUTTON_LONG_PRESS                     BIT(2)
 
 static enum button_status {
     released = 0,
     pressed,
 };
 
-static struct k_work_q btn_work_q;
+static struct k_work_q button_work_q;
+static struct k_work bt_button_work;
 static struct gpio_callback bt_button_cb;
 static struct gpio_dt_spec bt_button = GPIO_DT_SPEC_GET(BT_BUTTON_NODE, gpios);
 static enum button_status bt_button_status = released;
 
 K_THREAD_STACK_DEFINE(btn_work_stack, BTN_QUEUE_STACK_SIZE);
+K_EVENT_DEFINE(bt_button_press_event);
 
 static void init_work_q();
 static int init_bt_button();
@@ -24,6 +31,7 @@ static int init_bt_button();
 static void bt_button_isr(const struct device *dt, struct gpio_callback *cb, uint32_t pins);
 static void on_pressed();
 static void on_released();
+static void check_pressed_time(struct k_work *item);
 
 
 int init_button_service() {
@@ -33,11 +41,12 @@ int init_button_service() {
    
 
 static void init_work_q() {
-    k_work_queue_init(&btn_work_q);
-
-    k_work_queue_start(&btn_work_q,
+    k_work_queue_init(&button_work_q);
+    k_work_queue_start(&button_work_q,
                        btn_work_stack, K_THREAD_STACK_SIZEOF(btn_work_stack),
                        BTN_WORKQUEUE_PRIORITY, NULL);
+
+    k_work_init(&bt_button_work, check_pressed_time);
 }
 
 static int init_bt_button() {
@@ -63,11 +72,9 @@ static int init_bt_button() {
 void bt_button_isr(const struct device *dt, struct gpio_callback *cb, uint32_t pins) {
     switch (bt_button_status){
     case pressed:
-        bt_button_status = released;
         on_released();
         break;
     case released:
-        bt_button_status = pressed;
         on_pressed();
         break;
     default:
@@ -76,9 +83,26 @@ void bt_button_isr(const struct device *dt, struct gpio_callback *cb, uint32_t p
 }
 
 static void on_pressed() {
-
+    bt_button_status = pressed;
+    k_work_submit(&bt_button_work);
 }
 
 static void on_released() {
+    bt_button_status = released;
+    k_event_set(&bt_button_press_event, BT_BUTTON_RELEASE);
+}
 
+static void check_pressed_time(struct k_work *item) {
+    int64_t start_time = k_uptime_get();
+    for (;;) {
+        int64_t time_delta = k_uptime_delta(&start_time);
+        if (time_delta > LONG_PRESS_BOUNDARY_MS) {  // Long Press
+            k_event_set(&bt_button_press_event, BT_BUTTON_LONG_PRESS);
+            return;
+        }
+        else if (bt_button_status == released) {  // Short Press
+            k_event_set(&bt_button_press_event, BT_BUTTON_SHORT_PRESS);
+            return ;
+        }
+    }
 }
