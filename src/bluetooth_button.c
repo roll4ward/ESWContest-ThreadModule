2eb9f42b50b69ea61ee99011b80a47782ab80e89
Lee Junhee <junhee0110@gmail.com>
2024-05-22 01:45:45 +0900

    [Feat] Bluetooth 버튼 짧은 클릭 핸들러 다시 추가

--- src/bluetooth_button.c
+++ src/bluetooth_button.c
@@ -33,6 +33,11 @@ static void bt_on_long(struct k_work *work) {
         }
 }
 
+static  bt_on_short(struct k_work *work) {
+        LOG_INF("bluetooth SHORT PRESSED");
+}
+
+
 struct action_button bt_button = {
     .dt_spec= GPIO_DT_SPEC_GET(BT_BUTTON_NODE, gpios),
     .press_status = released,
@@ -40,5 +45,5 @@ struct action_button bt_button = {
 
 struct action_button_callback bt_callback = {
         .on_long_press = bt_on_long,
-        .on_short_press = NULL,
+        .on_short_press = bt_on_short,
 };
