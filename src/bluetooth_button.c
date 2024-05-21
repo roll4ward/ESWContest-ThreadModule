28ccc700ce120318a839bb419f176da0fa60f441
Lee Junhee <junhee0110@gmail.com>
2024-05-22 01:32:25 +0900

    [Feat] 블루투스 버튼 로직 추가

--- src/bluetooth_button.c
+++ src/bluetooth_button.c
@@ -4,14 +4,34 @@
 
 #define     BT_BUTTON_NODE                           DT_ALIAS(bluetooth_button)
 
+static enum bt_status {
+        waiting,
+        advertise,
+        connected,
+};
+
+enum bt_status status = waiting;
+
+static void do_advertise();
+static void do_waiting();
+static void do_unconnect();
+
 static void bt_on_long(struct k_work *work) {
         LOG_INF("bluetooth LONG PRESSED");
-}
+        switch(status) {
+                case waiting:
+                do_advertise();
+                break;
 
-static  bt_on_short(struct k_work *work) {
-        LOG_INF("bluetooth SHORT PRESSED");
-}
+                case advertise:
+                do_waiting();
+                break;
 
+                case connected:
+                do_unconnect();
+                break;
+        }
+}
 
 struct action_button bt_button = {
     .dt_spec= GPIO_DT_SPEC_GET(BT_BUTTON_NODE, gpios),
@@ -20,5 +40,5 @@ struct action_button bt_button = {
 
 struct action_button_callback bt_callback = {
         .on_long_press = bt_on_long,
-        .on_short_press = bt_on_short,
+        .on_short_press = NULL,
 };
