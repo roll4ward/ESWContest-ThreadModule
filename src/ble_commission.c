b7c5bff65839e358ed3317b998fe0ee1c4698b12
Lee Junhee <junhee0110@gmail.com>
2024-07-15 15:21:32 +0900

    [Refac.] ADD common file, USE Macro
    - 공통 파일 만들어서 공통 함수 몰아두기
    - UUID 헤더 파일 분리했음
    - MACRO 사용해서 공통된 형식 길이 줄임
    - command callback 작게 나눌 예정

--- src/ble_commission.c
+++ src/ble_commission.c
@@ -11,22 +11,7 @@
 #include <openthread/instance.h>
 
 #include "ble_commissioning.h"
-
-#define BT_UUID_COMMISSION_SERVICE_VAL                           BT_UUID_128_ENCODE(0x9fff0001,0x89f6, 0x4f1b, 0x9e5d, 0x0d4648d944c9)
-#define BT_UUID_COMMISSION_NETWORK_NAME_VAL                      BT_UUID_128_ENCODE(0x9fff0002,0x89f6, 0x4f1b, 0x9e5d, 0x0d4648d944c9)
-#define BT_UUID_COMMISSION_NETWORK_KEY_VAL                       BT_UUID_128_ENCODE(0x9fff0003,0x89f6, 0x4f1b, 0x9e5d, 0x0d4648d944c9)
-#define BT_UUID_COMMISSION_EXT_PANID_VAL                         BT_UUID_128_ENCODE(0x9fff0004,0x89f6, 0x4f1b, 0x9e5d, 0x0d4648d944c9)
-#define BT_UUID_COMMISSION_COMMAND_VAL                           BT_UUID_128_ENCODE(0x9fff0005,0x89f6, 0x4f1b, 0x9e5d, 0x0d4648d944c9)
-#define BT_UUID_COMMISSION_STATUS_VAL                            BT_UUID_128_ENCODE(0x9fff0006,0x89f6, 0x4f1b, 0x9e5d, 0x0d4648d944c9)
-#define BT_UUID_COMMISSION_ROLE_VAL                              BT_UUID_128_ENCODE(0x9fff0007,0x89f6, 0x4f1b, 0x9e5d, 0x0d4648d944c9)
-
-#define BT_UUID_COMMISSION_SERVICE                               BT_UUID_DECLARE_128(BT_UUID_COMMISSION_SERVICE_VAL)
-#define BT_UUID_COMMISSION_NETWORK_NAME                          BT_UUID_DECLARE_128(BT_UUID_COMMISSION_NETWORK_NAME_VAL)
-#define BT_UUID_COMMISSION_NETWORK_KEY                           BT_UUID_DECLARE_128(BT_UUID_COMMISSION_NETWORK_KEY_VAL)
-#define BT_UUID_COMMISSION_EXT_PANID                             BT_UUID_DECLARE_128(BT_UUID_COMMISSION_EXT_PANID_VAL)
-#define BT_UUID_COMMISSION_COMMAND                               BT_UUID_DECLARE_128(BT_UUID_COMMISSION_COMMAND_VAL)
-#define BT_UUID_COMMISSION_STATUS                                BT_UUID_DECLARE_128(BT_UUID_COMMISSION_STATUS_VAL)
-#define BT_UUID_COMMISSION_ROLE                                  BT_UUID_DECLARE_128(BT_UUID_COMMISSION_ROLE_VAL)
+#include "ble_uuid.h"
 
 LOG_MODULE_REGISTER(ble_create_network, LOG_LEVEL_DBG);
 
@@ -40,32 +25,18 @@ USER_DATA_INFO(status, commission_status, 1U, WAITING);
 USER_DATA_INFO(otDeviceRole, role, 1U, OT_DEVICE_ROLE_DISABLED);
 
 // Indicate Enable
-static bool status_indicate_enabled = false;
-static bool role_indicate_enabled = false;
+INDICATE_DEFINE(commission_status, BT_UUID_COMMISSION_STATUS);
+INDICATE_DEFINE(role, BT_UUID_COMMISSION_ROLE);
 
 // Workqueue Variables
-static void create_new_network(struct k_work *work);
-static void join_network(struct k_work *work);
-static void reset_dataset(struct k_work *work);
-
-K_WORK_DEFINE(create_new_work, create_new_network);
-K_WORK_DEFINE(join_work, join_network);
-K_WORK_DEFINE(reset_dataset_work, reset_dataset);
+COMMAND_WORK_DECLARE(create_new_network);
+COMMAND_WORK_DECLARE(join_network);
+COMMAND_WORK_DECLARE(reset_dataset);
 
 static ssize_t write_command(struct bt_conn *conn,
                                   const struct bt_gatt_attr *attr,
                                   const void *buf, uint16_t len,
                                   uint16_t offset, uint8_t flags);
-
-static ssize_t write_gatt(struct bt_conn *conn,
-                                  const struct bt_gatt_attr *attr,
-                                  const void *buf, uint16_t len,
-                                  uint16_t offset, uint8_t flags);
-
-static void status_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value);
-static void role_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value);
-
-static int commission_state_indicate(uint8_t state);                               
 
 BT_GATT_SERVICE_DEFINE(
     thread_commission_service,
@@ -90,41 +61,13 @@ BT_GATT_SERVICE_DEFINE(
                            BT_GATT_CHRC_READ | BT_GATT_CHRC_INDICATE,
                            BT_GATT_PERM_READ,
                            read_gatt, NULL, &commission_status),
-    BT_GATT_CCC(status_ccc_cfg_changed, BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),                      
+    BT_GATT_CCC(INDICATE_CCC_CALLBACK(commission_status), BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),                      
     BT_GATT_CHARACTERISTIC(BT_UUID_COMMISSION_ROLE,
                            BT_GATT_CHRC_READ | BT_GATT_CHRC_INDICATE,
                            BT_GATT_PERM_READ,
                            read_gatt, NULL, &role),
-    BT_GATT_CCC(role_ccc_cfg_changed, BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+    BT_GATT_CCC(INDICATE_CCC_CALLBACK(role), BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
 );
-
-ssize_t read_gatt(struct bt_conn *conn,
-                                 const struct bt_gatt_attr *attr,
-                                 void *buf, uint16_t len, 
-                                 uint16_t offset) {
-    user_data_info *info = (user_data_info *)attr->user_data;
-    LOG_HEXDUMP_DBG(((struct bt_uuid_128 *)BT_UUID_128(attr->uuid))->val, 16U, "Data read. UUID :");
-    return bt_gatt_attr_read(conn, attr, buf, len, offset, info->data, info->len);                                
-}
-
-static ssize_t write_gatt(struct bt_conn *conn,
-                                  const struct bt_gatt_attr *attr,
-                                  const void *buf, uint16_t len,
-                                  uint16_t offset, uint8_t flags) {
-    user_data_info *info = (user_data_info *)attr->user_data;
-    if (len != info->len) {
-        return BT_GATT_ERR(BT_ATT_ERR_INVALID_ATTRIBUTE_LEN);
-    }
-
-    if (offset != 0) {
-        return BT_GATT_ERR(BT_ATT_ERR_INVALID_OFFSET);
-    }
-
-    memcpy(info->data, buf, len);
-    LOG_HEXDUMP_DBG(((struct bt_uuid_128 *)BT_UUID_128(attr->uuid))->val, 16U, "Data Write. UUID :");
-    LOG_HEXDUMP_DBG(info->data, info->len, "Value :");
-    return len;
-}
 
 static ssize_t write_command(struct bt_conn *conn,
                                   const struct bt_gatt_attr *attr,
@@ -141,17 +84,17 @@ static ssize_t write_command(struct bt_conn *conn,
     switch (*((uint8_t *)buf)) {
         case NEW_NETWORK:
             LOG_DBG("COMMAND SET : CREATE NEW");
-            k_work_submit(&create_new_work);
+            k_work_submit(&COMMAND_WORK_NAME(create_new_network));
             break;
 
         case JOIN_NETWORK:
             LOG_DBG("COMMAND SET : JOIN");
-            k_work_submit(&join_work);
+            k_work_submit(&COMMAND_WORK_NAME(join_network));
             break;
 
         case RESET_DATASET:
             LOG_DBG("COMMAND SET : RESET");
-            k_work_submit(&reset_dataset_work);
+            k_work_submit(&COMMAND_WORK_NAME(reset_dataset));
             break;
 
         default:
@@ -162,48 +105,7 @@ static ssize_t write_command(struct bt_conn *conn,
     return len;
 }
 
-static void status_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value) {
-    status_indicate_enabled = (value == BT_GATT_CCC_INDICATE);
-}
-
-static void role_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value) {
-    role_indicate_enabled = (value == BT_GATT_CCC_INDICATE);
-}
-
-static int commission_state_indicate(uint8_t state) {
-    struct bt_gatt_indicate_params ind_params = {
-        .uuid = BT_UUID_COMMISSION_STATUS,
-        .data = commission_status.data,
-        .len = commission_status.len
-    };
-
-    USER_DATA_ORIGIN(commission_status) = state;
-
-    if (!status_indicate_enabled) {
-		return -EACCES;
-	}
-
-	return bt_gatt_indicate(NULL, &ind_params);
-}
-
-static int role_indicate() {
-    struct bt_gatt_indicate_params ind_params = {
-        .uuid = BT_UUID_COMMISSION_ROLE,
-        .data = role.data,
-        .len = role.len
-    };
-
-    USER_DATA_ORIGIN(role) = otThreadGetDeviceRole(openthread_get_default_instance());
-    LOG_DBG("role : %d", USER_DATA_ORIGIN(role));
-
-    if (!role_indicate_enabled) {
-		return -EACCES;
-	}
-
-	return bt_gatt_indicate(NULL, &ind_params);
-}
-
-static void create_new_network(struct k_work *work) {
+COMMAND_WORK_HANDLER(create_new_network) {
     otOperationalDataset new_dataset;
     otError err;
     
@@ -212,25 +114,25 @@ static void create_new_network(struct k_work *work) {
     otThreadSetEnabled(openthread_get_default_instance(), false);
 
     LOG_DBG("Let's Create New NETWORK");
-    commission_state_indicate(PROGRESSING);
+    INDICATE_VALUE(commission_status, PROGRESSING);
 
     err = otDatasetCreateNewNetwork(openthread_get_default_instance(), &new_dataset);
     
     if (err != OT_ERROR_NONE) {
         LOG_ERR("FAILED: Create NEW Network");
-        commission_state_indicate(FAILED);
+        INDICATE_VALUE(commission_status, FAILED);
         return;
     }
 
-    memcpy(&new_dataset.mNetworkName, &USER_DATA_ORIGIN(networkname), sizeof(otNetworkName));
-    memcpy(&USER_DATA_ORIGIN(networkkey), &new_dataset.mNetworkKey, sizeof(otNetworkKey));
-    memcpy(&USER_DATA_ORIGIN(extpanid), &new_dataset.mExtendedPanId, sizeof(otExtendedPanId));
+    memcpy(&new_dataset.mNetworkName, &USER_DATA(networkname), sizeof(otNetworkName));
+    memcpy(&USER_DATA(networkkey), &new_dataset.mNetworkKey, sizeof(otNetworkKey));
+    memcpy(&USER_DATA(extpanid), &new_dataset.mExtendedPanId, sizeof(otExtendedPanId));
 
     err = otDatasetSetActive(openthread_get_default_instance(), &new_dataset);
 
     if (err != OT_ERROR_NONE) {
         LOG_ERR("FAILED: Create NEW Network");
-        commission_state_indicate(FAILED);
+        INDICATE_VALUE(commission_status, FAILED);
         return;
     }
 
@@ -238,10 +140,10 @@ static void create_new_network(struct k_work *work) {
     otThreadSetEnabled(openthread_get_default_instance(), true);
     openthread_api_mutex_unlock(openthread_get_default_context());
 
-    commission_state_indicate(DONE);
+    INDICATE_VALUE(commission_status, DONE);
 }
 
-static void join_network(struct k_work *work) {\
+COMMAND_WORK_HANDLER(join_network) {
     otError err;
     otOperationalDataset join_dataset = {0};
     
@@ -250,18 +152,19 @@ static void join_network(struct k_work *work) {\
     otThreadSetEnabled(openthread_get_default_instance(), false);
 
     LOG_DBG("Let's Join New NETWORK");
-    commission_state_indicate(PROGRESSING);
+    INDICATE_VALUE(commission_status, PROGRESSING);
 
-    memcpy(&join_dataset.mNetworkName, &USER_DATA_ORIGIN(networkname), sizeof(otNetworkName));
+    memcpy(&join_dataset.mNetworkName, &USER_DATA(networkname), sizeof(otNetworkName));
     join_dataset.mComponents.mIsNetworkNamePresent = true;
-    memcpy(&join_dataset.mNetworkKey, &USER_DATA_ORIGIN(networkkey), sizeof(otNetworkKey));
+    memcpy(&join_dataset.mNetworkKey, &USER_DATA(networkkey), sizeof(otNetworkKey));
     join_dataset.mComponents.mIsNetworkKeyPresent = true;
-    memcpy(&join_dataset.mExtendedPanId, &USER_DATA_ORIGIN(extpanid), sizeof(otExtendedPanId));
+    memcpy(&join_dataset.mExtendedPanId, &USER_DATA(extpanid), sizeof(otExtendedPanId));
     join_dataset.mComponents.mIsExtendedPanIdPresent = true;
 
     err = otDatasetSetActive(openthread_get_default_instance(), &join_dataset);
     if (err != OT_ERROR_NONE) {
         LOG_ERR("cannot update DATASET");
+        INDICATE_VALUE(commission_status, FAILED);
         return;
     }
 
@@ -270,22 +173,22 @@ static void join_network(struct k_work *work) {\
     otThreadSetEnabled(openthread_get_default_instance(), true);
     openthread_api_mutex_unlock(openthread_get_default_context());
 
-    commission_state_indicate(DONE);
+    INDICATE_VALUE(commission_status, DONE);
 }
 
-static void reset_dataset(struct k_work *work) {
-    commission_state_indicate(PROGRESSING);
+COMMAND_WORK_HANDLER(reset_dataset) {
+    INDICATE_VALUE(commission_status, PROGRESSING);
 
-    memset(&USER_DATA_ORIGIN(networkname), 0, sizeof(otNetworkName));
-    memset(&USER_DATA_ORIGIN(networkkey), 0, sizeof(otNetworkKey));
-    memset(&USER_DATA_ORIGIN(extpanid), 0, sizeof(otExtendedPanId));
+    memset(&USER_DATA(networkname), 0, sizeof(otNetworkName));
+    memset(&USER_DATA(networkkey), 0, sizeof(otNetworkKey));
+    memset(&USER_DATA(extpanid), 0, sizeof(otExtendedPanId));
     
-    commission_state_indicate(DONE);
+    INDICATE_VALUE(commission_status, DONE);
 }
 
 static void state_changed_cb(otChangedFlags flag, void *context) {
     if (flag & OT_CHANGED_THREAD_ROLE) {
-        role_indicate();
+        INDICATE_VALUE(role, otThreadGetDeviceRole(openthread_get_default_instance()));
     }
 }
 
--- src/ble_scanning.c
+++ src/ble_scanning.c
@@ -8,20 +8,7 @@
 #include <openthread/thread.h>
 
 #include "ble_commissioning.h"
-
-#define BT_UUID_SCAN_SERVICE_VAL                                 BT_UUID_128_ENCODE(0x9fff0008,0x89f6, 0x4f1b, 0x9e5d, 0x0d4648d944c9)
-#define BT_UUID_SCAN_COMMAND_VAL                                 BT_UUID_128_ENCODE(0x9fff0009,0x89f6, 0x4f1b, 0x9e5d, 0x0d4648d944c9)
-#define BT_UUID_SCAN_NETWORK_NAME_VAL                            BT_UUID_128_ENCODE(0x9fff000a,0x89f6, 0x4f1b, 0x9e5d, 0x0d4648d944c9)
-#define BT_UUID_SCAN_EXT_PANID_VAL                               BT_UUID_128_ENCODE(0x9fff000b,0x89f6, 0x4f1b, 0x9e5d, 0x0d4648d944c9)
-#define BT_UUID_SCAN_STATUS_VAL                                  BT_UUID_128_ENCODE(0x9fff000c,0x89f6, 0x4f1b, 0x9e5d, 0x0d4648d944c9)
-#define BT_UUID_SCAN_LENGTH_VAL                                  BT_UUID_128_ENCODE(0x9fff000d,0x89f6, 0x4f1b, 0x9e5d, 0x0d4648d944c9)
-
-#define BT_UUID_SCAN_SERVICE                                     BT_UUID_DECLARE_128(BT_UUID_SCAN_SERVICE_VAL)
-#define BT_UUID_SCAN_COMMAND                                     BT_UUID_DECLARE_128(BT_UUID_SCAN_COMMAND_VAL)
-#define BT_UUID_SCAN_NETWORK_NAME                                BT_UUID_DECLARE_128(BT_UUID_SCAN_NETWORK_NAME_VAL)
-#define BT_UUID_SCAN_EXT_PANID                                   BT_UUID_DECLARE_128(BT_UUID_SCAN_EXT_PANID_VAL)
-#define BT_UUID_SCAN_STATUS                                      BT_UUID_DECLARE_128(BT_UUID_SCAN_STATUS_VAL)
-#define BT_UUID_SCAN_LENGTH                                      BT_UUID_DECLARE_128(BT_UUID_SCAN_LENGTH_VAL)
+#include "ble_uuid.h"
 
 LOG_MODULE_REGISTER(ble_thread_scan, LOG_LEVEL_INF);
 
@@ -32,39 +19,24 @@ USER_DATA_INFO(status, scan_status, 1U, WAITING);
 USER_DATA_INFO(uint8_t, length, 1U, 0);
 
 // Workqueue
-static void scan(struct k_work *work);
-static void reset_queue(struct k_work *work);
-static void get_result(struct k_work *work);
-
-K_WORK_DEFINE(scan_work, scan);
-K_WORK_DEFINE(reset_queue_work, reset_queue);
-K_WORK_DEFINE(get_result_work, get_result);
+COMMAND_WORK_DECLARE(scan);
+COMMAND_WORK_DECLARE(reset_queue);
+COMMAND_WORK_DECLARE(get_result);
 
 // Scan Result Queue
+K_QUEUE_DEFINE(scan_result_queue);
 static void add_scan_result_to_queue(otActiveScanResult *aResult, void *aContext);
 
-K_QUEUE_DEFINE(scan_result_queue);
-
 // Indicate Enable Variable
-static bool network_name_indicate_enabled = false;
-static bool ext_panid_indicate_enabled = false;
-static bool status_indicate_enabled = false;
-static bool length_indicate_enabled = false;
+INDICATE_DEFINE(scan_networkname, BT_UUID_SCAN_NETWORK_NAME);
+INDICATE_DEFINE(scan_extpanid, BT_UUID_SCAN_EXT_PANID);
+INDICATE_DEFINE(scan_status, BT_UUID_SCAN_STATUS);
+INDICATE_DEFINE(length, BT_UUID_SCAN_LENGTH);
 
 static ssize_t write_command(struct bt_conn *conn,
                                   const struct bt_gatt_attr *attr,
                                   const void *buf, uint16_t len,
                                   uint16_t offset, uint8_t flags);
-
-static void network_name_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value);
-static void ext_panid_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value);
-static void status_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value);
-static void length_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value);
-
-static int network_name_indicate();
-static int ext_panid_indicate();
-static int status_indicate(uint8_t state);
-static int length_indicate();
 
 BT_GATT_SERVICE_DEFINE(
     thread_scan_service,
@@ -76,23 +48,23 @@ BT_GATT_SERVICE_DEFINE(
     BT_GATT_CHARACTERISTIC(BT_UUID_SCAN_NETWORK_NAME,
                            BT_GATT_CHRC_READ | BT_GATT_CHRC_INDICATE,
                            BT_GATT_PERM_READ,
-                           read_gatt, NULL, &USER_DATA_ORIGIN(scan_networkname)),
-    BT_GATT_CCC(network_name_ccc_cfg_changed, BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+                           read_gatt, NULL, &USER_DATA(scan_networkname)),
+    BT_GATT_CCC(INDICATE_CCC_CALLBACK(scan_networkname), BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
     BT_GATT_CHARACTERISTIC(BT_UUID_SCAN_EXT_PANID,
                            BT_GATT_CHRC_READ | BT_GATT_CHRC_INDICATE,
                            BT_GATT_PERM_READ,
-                           read_gatt, NULL, &USER_DATA_ORIGIN(scan_extpanid)),
-    BT_GATT_CCC(ext_panid_ccc_cfg_changed, BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),                       
+                           read_gatt, NULL, &USER_DATA(scan_extpanid)),
+    BT_GATT_CCC(INDICATE_CCC_CALLBACK(scan_extpanid), BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),                       
     BT_GATT_CHARACTERISTIC(BT_UUID_SCAN_STATUS,
                            BT_GATT_CHRC_READ | BT_GATT_CHRC_INDICATE,
                            BT_GATT_PERM_READ,
-                           read_gatt, NULL, &USER_DATA_ORIGIN(scan_status)),
-    BT_GATT_CCC(status_ccc_cfg_changed, BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+                           read_gatt, NULL, &USER_DATA(scan_status)),
+    BT_GATT_CCC(INDICATE_CCC_CALLBACK(scan_status), BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
     BT_GATT_CHARACTERISTIC(BT_UUID_SCAN_LENGTH,
                            BT_GATT_CHRC_READ | BT_GATT_CHRC_INDICATE,
                            BT_GATT_PERM_READ,
-                           read_gatt, NULL, &USER_DATA_ORIGIN(length)),
-    BT_GATT_CCC(length_ccc_cfg_changed, BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+                           read_gatt, NULL, &USER_DATA(length)),
+    BT_GATT_CCC(INDICATE_CCC_CALLBACK(length), BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
 );
 
 static ssize_t write_command(struct bt_conn *conn,
@@ -110,17 +82,17 @@ static ssize_t write_command(struct bt_conn *conn,
     switch (*((uint8_t *)buf)) {
         case SCAN:
             LOG_DBG("COMMAND SET : SCAN");
-            k_work_submit(&scan_work);
+            k_work_submit(&COMMAND_WORK_NAME(scan));
             break;
 
         case RESET_QUEUE:
             LOG_DBG("COMMAND SET : RESET_QUEUE");
-            k_work_submit(&reset_queue_work);
+            k_work_submit(&COMMAND_WORK_NAME(reset_queue));
             break;
 
         case GET:
             LOG_DBG("COMMAND SET : GET");
-            k_work_submit(&get_result_work);
+            k_work_submit(&COMMAND_WORK_NAME(get_result));
             break;
 
         default:
@@ -131,91 +103,17 @@ static ssize_t write_command(struct bt_conn *conn,
     return len;
 }
 
-static void network_name_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value) {
-    network_name_indicate_enabled = (value == BT_GATT_CCC_INDICATE);
-}
-
-static void ext_panid_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value) {
-    ext_panid_indicate_enabled = (value == BT_GATT_CCC_INDICATE);
-}
-
-static void status_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value) {
-    status_indicate_enabled = (value == BT_GATT_CCC_INDICATE);
-}
-
-static void length_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value) {
-    length_indicate_enabled = (value == BT_GATT_CCC_INDICATE);
-}
-
-static int network_name_indicate() {
-    if (!network_name_indicate_enabled) {
-		return -EACCES;
-	}
-
-    struct bt_gatt_indicate_params ind_params = {
-        .uuid = BT_UUID_SCAN_NETWORK_NAME,
-        .data = scan_networkname.data,
-        .len = scan_networkname.len
-    };
-
-	return bt_gatt_indicate(NULL, &ind_params);
-}
-
-static int ext_panid_indicate() {
-    if (!ext_panid_indicate_enabled) {
-		return -EACCES;
-	}
-
-    struct bt_gatt_indicate_params ind_params = {
-        .uuid = BT_UUID_SCAN_EXT_PANID,
-        .data = scan_extpanid.data,
-        .len = scan_extpanid.len
-    };
-
-	return bt_gatt_indicate(NULL, &ind_params);
-}
-
-static int status_indicate(uint8_t state) {
-    USER_DATA_ORIGIN(scan_status) = state;
-
-    if (!status_indicate_enabled) {
-		return -EACCES;
-	}
-
-    struct bt_gatt_indicate_params ind_params = {
-        .uuid = BT_UUID_SCAN_STATUS,
-        .data = scan_status.data,
-        .len = scan_status.len
-    };
-
-	return bt_gatt_indicate(NULL, &ind_params);
-}
-
-static int length_indicate() {
-    if (!length_indicate_enabled) {
-		return -EACCES;
-	}
-
-    struct bt_gatt_indicate_params ind_params = {
-        .uuid = BT_UUID_SCAN_LENGTH,
-        .data = length.data,
-        .len = length.len
-    };
-
-	return bt_gatt_indicate(NULL, &ind_params);
-}
-
-static void scan(struct k_work *work) {
+COMMAND_WORK_HANDLER(scan) {
     otError err;
     
     LOG_DBG("Start Scan");
-    status_indicate(PROGRESSING);
+    INDICATE_VALUE(scan_status, PROGRESSING);
 
     otIp6SetEnabled(openthread_get_default_instance(), true);
     err = otThreadDiscover(openthread_get_default_instance(), 0xffffffff, OT_PANID_BROADCAST, false, false, add_scan_result_to_queue, NULL);
 
     if (err != OT_ERROR_NONE) {
-        status_indicate(FAILED);
+        INDICATE_VALUE(scan_status, FAILED);
         LOG_ERR("SCAN FAILED : %d", err);
     }
 }
@@ -228,14 +126,14 @@ static void add_scan_result_to_queue(otActiveScanResult *aResult, void *aContext
 
     if (!aResult->mDiscover) {
         LOG_DBG("Scan end");
-        status_indicate(DONE);
+        INDICATE_VALUE(scan_status, DONE);
         return;
     }
 
     otActiveScanResult *result = k_malloc(sizeof(otActiveScanResult));
     if(!result) {
         LOG_ERR("CAN NOT Allocate memory for scan result");
-        status_indicate(FAILED);
+        INDICATE_VALUE(scan_status, FAILED);
         return;
     }
 
@@ -243,49 +141,50 @@ static void add_scan_result_to_queue(otActiveScanResult *aResult, void *aContext
 
     k_queue_append(&scan_result_queue, result);
 
-    ++(USER_DATA_ORIGIN(length));
-    length_indicate();
+    ++(USER_DATA(length));
+    INDICATE(length);
 
-    LOG_DBG("Add Result : %s, length = %d", result->mNetworkName.m8, USER_DATA_ORIGIN(length));
+    LOG_DBG("Add Result : %s, length = %d", result->mNetworkName.m8, USER_DATA(length));
 }
 
-static void reset_queue(struct k_work *work) {
+COMMAND_WORK_HANDLER(reset_queue) {
     LOG_DBG("Reset Queue");
-    status_indicate(PROGRESSING);
+    INDICATE_VALUE(scan_status, PROGRESSING);
     
     while (!k_queue_is_empty(&scan_result_queue)) {
         k_free(k_queue_get(&scan_result_queue, K_NO_WAIT));
         LOG_DBG("DELETE ITEM of QUEUE");
     }
 
-    USER_DATA_ORIGIN(length) = 0;
-    length_indicate();
+    USER_DATA(length) = 0;
+    INDICATE(length);
 
-    status_indicate(DONE);
-    LOG_DBG("RESET QUEUE DONE, length = %d", USER_DATA_ORIGIN(length));
+    INDICATE_VALUE(scan_status, DONE);
+
+    LOG_DBG("RESET QUEUE DONE, length = %d", USER_DATA(length));
 }
 
-static void get_result(struct k_work *work) {
+COMMAND_WORK_HANDLER(get_result) {
     LOG_DBG("get result");
-    status_indicate(PROGRESSING);
+    INDICATE_VALUE(scan_status, PROGRESSING);
 
     otActiveScanResult *result = k_queue_get(&scan_result_queue, K_NO_WAIT);
     if (!result) {
-        status_indicate(FAILED);
+        INDICATE_VALUE(scan_status, FAILED);
         LOG_DBG("No Data in Queue, empty ? = %d", k_queue_is_empty(&scan_result_queue));
         return;
     }
 
-    --USER_DATA_ORIGIN(length);
-    length_indicate();
+    --USER_DATA(length);
+    INDICATE(length);
 
-    LOG_DBG("Get Result : %s, length = %d", result->mNetworkName.m8, USER_DATA_ORIGIN(length));
+    LOG_DBG("Get Result : %s, length = %d", result->mNetworkName.m8, USER_DATA(length));
 
-    memcpy(&USER_DATA_ORIGIN(scan_networkname), &result->mNetworkName, sizeof(otNetworkName));
-    memcpy(&USER_DATA_ORIGIN(scan_extpanid), &result->mExtendedPanId, sizeof(otExtendedPanId));
+    memcpy(&USER_DATA(scan_networkname), &result->mNetworkName, sizeof(otNetworkName));
+    memcpy(&USER_DATA(scan_extpanid), &result->mExtendedPanId, sizeof(otExtendedPanId));
     k_free(result);
 
-    network_name_indicate();
-    ext_panid_indicate();
-    status_indicate(DONE);
+    INDICATE(scan_networkname);
+    INDICATE(scan_extpanid);
+    INDICATE_VALUE(scan_status, DONE);
 }
